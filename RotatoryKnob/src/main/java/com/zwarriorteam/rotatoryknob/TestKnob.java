e00bc225d15737a367b2c707f9f7b8cd83598518
Sujan <suz_47@outlook.com>
2021-01-14 12:15:10 +0545

    Rotatory Knob Project - First Commit

---
+++ RotatoryKnob/src/main/java/com/zwarriorteam/rotatoryknob/TestKnob.java
@@ -0,0 +1,10 @@
+package com.zwarriorteam.rotatoryknob;
+
+import android.util.Log;
+
+public class TestKnob {
+    private static final String TAG = "TestKnob";
+    public static void log_str(String str){
+        Log.e(TAG, str);
+    }
+}
