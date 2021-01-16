3cd0b506a88a029680b172515d456c64bc630dfc
Sujan <suz_47@outlook.com>
2021-01-17 02:07:30 +0545

    Update Rotatory KnobView added multiple features.
    (cherry picked from commit edd38db52dd73cfefa7f69fef957a921cc00429e)

---
+++ app/src/main/java/com/rotatoryknobexample/RotatoryKnobView.java
@@ -0,0 +1,12 @@
+package com.rotatoryknobexample;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
